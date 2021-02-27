c575012265caaaa216235cc257e5e93d7800f173
PieterRuanCronje <cronjepieterruan@gmail.com>
2021-02-27 14:23:20 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	count := 1
+	for {
+		time.Sleep(10 * time.Millisecond)
+		if isPrime(count) {
+			fmt.Println(count)
+		}
+		count++
+	}
+}
+
+func isPrime(num int) bool {
+	half := num / 2
+
+	if num == 1 || num == 2 {
+		return true
+	}
+
+	for i := 2; i <= half; i++ {
+		if num%i == 0 {
+			return false
+		}
+	}
+
+	return true
+
+}
